e613fec4ae512c008c6f29b46ffeb1d32c3afc30
ayocodingit <ayocodingit@gmail.com>
2021-09-24 17:17:49 +0700

    rollback to before refactor v1

--- send/request.js
+++
@@ -1,8 +0,0 @@
-import request from 'request'
-import util from 'util'
-
-const sendRequest = () => {
-  return util.promisify(request)
-}
-
-export default sendRequest()
--- send/telegram.js
+++ send/telegram.js
@@ -6,7 +6,6 @@ import captureScreenshot from '../capture/screenshot.js'
 import { sendBodyIsValid } from '../send/elastic.js'
 import Config from '../config/index.js'
 import { messageValid } from '../template/message.js'
-import sendRequest from './request.js'
 
 const CHAT_ID = Number(Config.get('chat.id'))
 const apiTelegram = `https://api.telegram.org/${Config.get('telegram.bot')}`
@@ -25,24 +24,29 @@ const sendMessageWithUser = async (message, replyToMsgId = null) => {
   )
 }
 
-const sendMessageWithBoth = async (message) => {
-  await sendRequest({
-    url: apiTelegram + '/sendMessage',
-    formData: {
-      chat_id: CHAT_ID,
-      text: message
-    }
-  })
-}
-
-const sendPhotoWithBoth = async (picture) => {
-    const response = await sendRequest({
-      method: 'POST',
-      preambleCRLF: true,
-      postambleCRLF: true,
-      url: apiTelegram + '/sendPhoto',
+const sendMessageWithBoth = (message) => {
+  request.post(
+    {
+      url: apiTelegram + '/sendMessage',
       formData: {
         chat_id: CHAT_ID,
+        text: message
+      }
+    },
+    function cb (err) {
+      if (err) {
+        return console.error('send message failed:', err)
+      }
+    }
+  )
+}
+
+const sendPhotoWithReplyMessage = (picture, message) => {
+  request.post(
+    {
+      url: apiTelegram + '/sendPhoto',
+      formData: {
+        chat_id: Number(CHAT_ID),
         photo: {
           value: fs.createReadStream(picture),
           options: {
@@ -51,12 +55,16 @@ const sendPhotoWithBoth = async (picture) => {
           }
         }
       }
-    })
-    console.log(response)
-    if (response.statusCode !== 200) throw new Error (response.statusMessage)
-    const body = JSON.parse(response.body)
-    const { message_id: messageId } = body.result
-    return messageId
+    },
+    function cb (err, response) {
+      if (err) {
+        return console.error('send photo failed:', err)
+      }
+      const body = JSON.parse(response.body)
+      const { message_id: messageId } = body.result
+      sendMessageWithUser(message, Number(messageId))
+    }
+  )
 }
 
 const sendTelegram = async (git, payload) => {
@@ -64,9 +72,10 @@ const sendTelegram = async (git, payload) => {
     const picture = await captureScreenshot(payload.url, git)
     const message = messageValid(payload)
     sendBodyIsValid(payload)
-    if (!picture) return await sendMessageWithBoth(message)
-    const messageId = await sendPhotoWithBoth(picture)
-    return await sendMessageWithUser(message, Number(messageId))
+    if (!picture) return sendMessageWithBoth(message)
+    sendPhotoWithReplyMessage(picture, message)
+    fs.unlinkSync(picture)
+    return true
   } catch (error) {
     console.log(error.message)
     throw error
