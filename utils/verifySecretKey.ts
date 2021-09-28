ec452653e6251af5565f513bf3554df94699cbc9
ayocodingit <ayocodingit@gmail.com>
2021-09-28 15:40:52 +0700

    lang credential failed

--- utils/verifySecretKey.ts
+++ utils/verifySecretKey.ts
@@ -1,7 +1,8 @@
 import config from '../config'
+import lang from './lang'
 
 const verifySecretKey = async (secret: string): Promise<void> => {
-  if (secret !== config.get('secret.key')) throw Error('Credential is invalid')
+  if (secret !== config.get('secret.key')) throw Error(lang.__('credential_failed'))
 }
 
 export default verifySecretKey
