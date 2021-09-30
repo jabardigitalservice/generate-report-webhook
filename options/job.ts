95cbad1b3ff6b915456375ad523bc04c7c2a2740
ayocodingit <ayocodingit@gmail.com>
2021-09-30 09:01:53 +0700

    update job options

--- options/job.ts
+++ options/job.ts
@@ -2,6 +2,7 @@ const gitOptions = {
   delay: 10000,
   attempts: 10,
   priority: 1,
+  removeOnFail: true,
   removeOnComplete: true,
   backoff: 5000,
   timeout: 30000
@@ -11,6 +12,7 @@ const elasticOptions = {
   delay: 30000,
   priority: 3,
   removeOnComplete: true,
+  removeOnFail: true,
   backoff: 5000,
   timeout: 5000
 }
