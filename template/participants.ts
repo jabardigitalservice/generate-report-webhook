0a5a753d701e8d9f2c19b0b955587011d680cafc
ayocodingit <ayocodingit@gmail.com>
2021-10-07 13:19:50 +0700

    clean code complexity

--- template/participants.ts
+++ template/participants.ts
@@ -1,4 +1,6 @@
+import captureException from '../capture/exception'
 import config from '../config'
+import lang from '../utils/lang'
 import request from '../utils/request'
 
 interface rows {
@@ -11,16 +13,27 @@ const participantsMapping = (rows: rows[], users: string[]): string => {
   let result = ''
   for (const user of users) {
     let isFound = false
-    for (const row of rows) {
-      if (row.git === user) {
-        result += `${row.telegram} `
-        isFound = true
-        break
-      }
-    }
-    if (!isFound) result += `${user} `
+    const rowSearch = searchRowsParticipants(rows, isFound, user, result)
+    if (!rowSearch.isFound) result += `${user} `
+    else result += rowSearch.result
   }
   return result
+}
+
+const searchRowsParticipants = (rows: rows[], isFound: boolean, user: string, result: string): {
+  result: string, isFound: boolean
+} => {
+  for (const row of rows) {
+    if (row.git === user) {
+      result += `${row.telegram} `
+      isFound = true
+      break
+    }
+  }
+  return {
+    result,
+    isFound
+  }
 }
 
 export default async (participants: string) => {
@@ -30,7 +43,10 @@ export default async (participants: string) => {
     url: `${config.get('excel.json.url')}`
   })
 
-  if (response.statusCode !== 200) return participants
+  if (response.statusCode !== 200) {
+    captureException(new Error(lang.__('excel_json_url_failed')))
+    return participants
+  }
   const body = JSON.parse(response.body)
   const rows: rows[] = body.rows
 
