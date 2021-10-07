c4f51a271dfe6cb90945b40539a8ae6688f664a0
ayocodingit <ayocodingit@gmail.com>
2021-10-07 18:38:54 +0700

    config env

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,10 @@ import config from 'env-dot-prop'
 import dotEnv from 'dotenv'
 dotEnv.config()
 
+if (!config.get('node.env')) dotEnv.config({
+  path: '../.env'
+})
+
 config.set('dir', 'tmp')
 
 export default config
